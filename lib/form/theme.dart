7fe0e30bc9eaf1be3c5990ae9509e20044491162
banlang222 <master39@126.com>
2022-12-27 12:30:40 +0800

    super_form调整样式，现在需要配合主题使用

---
+++ lib/form/theme.dart
@@ -0,0 +1,19 @@
+import 'package:flutter/material.dart';
+
+ThemeData themeData = ThemeData(
+  textTheme: const TextTheme(subtitle1: TextStyle(color: Colors.black)),
+  inputDecorationTheme: InputDecorationTheme(
+    border: OutlineInputBorder(
+        borderSide: BorderSide(color: Colors.grey[700]!),
+        borderRadius: const BorderRadius.all(Radius.circular(8))),
+    enabledBorder: OutlineInputBorder(
+        borderSide: BorderSide(color: Colors.grey[700]!),
+        borderRadius: const BorderRadius.all(Radius.circular(8))),
+    disabledBorder: OutlineInputBorder(
+        borderSide: BorderSide(color: Colors.grey[700]!),
+        borderRadius: const BorderRadius.all(Radius.circular(8))),
+    focusedBorder: const OutlineInputBorder(
+        borderSide: BorderSide(color: Colors.amber),
+        borderRadius: BorderRadius.all(Radius.circular(8))),
+  ),
+);
